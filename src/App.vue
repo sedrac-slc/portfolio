28ca7f3d461f48bb67226f62d7f5beda0a5f5bad
sedrac-slc <slcsedrac@gmail.com>
2025-05-26 16:15:40 +0100

    resolve problem in impor

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { SidebarProvider } from './components/ui/sidebar';
   <!-- :style="{'--sidebar-width': '350px',}" -->
   <SidebarProvider class="dark:bg-gray-900" :style="{ '--sidebar-width': '355px', }">
     <LeftSidebar/>
-    <SidebarInset class="p-3 md:p-6 ">
+    <SidebarInset class="p-3 md:p-6 w-full bg-gray-200 dark:bg-slate-800">
 
     </SidebarInset>
     <RightSidebar />
